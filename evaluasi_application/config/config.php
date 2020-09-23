4da2bd93c6509c52433292d2ed22c23509f1186f
Agus Sumarna <sumarna.agus@gmail.com>
2020-09-23 16:19:37 +0700

    update config.php

--- evaluasi_application/config/config.php
+++ evaluasi_application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url']='http://localhost/webevaluasi';
+$config['base_url']='http://localhost/evaluasi';
 $config['fonts_path']= 'assets/fonts/';
 
 /*
